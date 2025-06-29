b015328e4ff1e5543e59e86da6cb8fdabfc50d02
michaelsue168 <michaelsue168@gmail.com>
2025-06-29 19:30:59 +0800

    為github page安裝套件

--- vite.config.js
+++ vite.config.js
@@ -7,6 +7,7 @@ import tailwindcss from "@tailwindcss/vite";
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: "/links-warehouse/",
   plugins: [vue(), vueJsx(), tailwindcss()],
   resolve: {
     alias: {
